2508b8519c90dddea625eecc508eb76f7e112ff0
Umut Topuz <umutopuz@gmail.com>
2023-11-29 13:34:37 +0300

    forgot password screen creation

--- src/app/main/pages/authentication/auth-forgot-password-v2/auth-forgot-password-v2.component.ts
+++ src/app/main/pages/authentication/auth-forgot-password-v2/auth-forgot-password-v2.component.ts
@@ -1,37 +1,19 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-
+import { Component, OnInit } from '@angular/core';
 import { CoreConfigService } from '@core/services/config.service';
+import { takeUntil, first } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-auth-forgot-password-v2',
   templateUrl: './auth-forgot-password-v2.component.html',
   styleUrls: ['./auth-forgot-password-v2.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class AuthForgotPasswordV2Component implements OnInit {
-  // Public
-  public emailVar;
+  public passwordTextType: boolean;
   public coreConfig: any;
-  public forgotPasswordForm: UntypedFormGroup;
-  public submitted = false;
-
-  // Private
   private _unsubscribeAll: Subject<any>;
 
-  /**
-   * Constructor
-   *
-   * @param {CoreConfigService} _coreConfigService
-   * @param {FormBuilder} _formBuilder
-   *
-   */
-  constructor(private _coreConfigService: CoreConfigService, private _formBuilder: UntypedFormBuilder) {
-    this._unsubscribeAll = new Subject();
-
+  constructor(private _coreConfigService: CoreConfigService,) {
     // Configure the layout
     this._coreConfigService.config = {
       layout: {
@@ -48,48 +30,23 @@ export class AuthForgotPasswordV2Component implements OnInit {
         enableLocalStorage: false
       }
     };
+
+    
+    
+    this._unsubscribeAll = new Subject();
+
   }
 
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.forgotPasswordForm.controls;
-  }
-
-  /**
-   * On Submit
-   */
-  onSubmit() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.forgotPasswordForm.invalid) {
-      return;
-    }
-  }
-
-  // Lifecycle Hooks
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * On init
-   */
   ngOnInit(): void {
-    this.forgotPasswordForm = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]]
-    });
 
-    // Subscribe to config changes
+    
     this._coreConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe(config => {
       this.coreConfig = config;
     });
   }
 
-  /**
-   * On destroy
-   */
-  ngOnDestroy(): void {
-    // Unsubscribe from all subscriptions
-    this._unsubscribeAll.next();
-    this._unsubscribeAll.complete();
+
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
   }
 }

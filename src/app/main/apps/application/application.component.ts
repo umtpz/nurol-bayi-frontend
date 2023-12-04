d7d4e599e09055f2578cd10e33cd826c42bd5f9b
Umut Topuz <umutopuz@gmail.com>
2023-12-04 13:23:22 +0300

    first 2 pages of application

--- src/app/main/apps/application/application.component.ts
+++ src/app/main/apps/application/application.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./application.component.scss']
 })
 export class ApplicationComponent implements OnInit {
-
+  page = 1
+  pageSection = 3
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  nextPage() {
+    if (this.page < 3) this.page++
+    this.pageSection = 0
+  }
+  prevPage() {
+    if (this.page > 0) this.page--
+    this.pageSection = 0
+  }
+  toPage(page) {
+    this.page = page
+    this.pageSection = 0
+  }
+  nextPageSection() {
+    if (this.pageSection < 3) this.pageSection++
+  }
+  prevPageSection() {
+    if (this.pageSection > 0) this.pageSection--
+  }
 }

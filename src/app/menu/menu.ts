608416195726eb714c7bd5e87d238ef9d394d390
Umut Topuz <umutopuz@gmail.com>
2023-12-21 12:49:58 +0300

    menu reduction

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -65,39 +65,39 @@ export const menu: CoreMenu[] = [
     ]
   },
   //* Dashboard
-  {
-    id: 'dashboard',
-    title: 'Dashboard',
-    translate: 'MENU.DASHBOARD.COLLAPSIBLE',
-    type: 'collapsible',
-    // role: ['Admin'], //? To hide collapsible based on user role
-    icon: 'home',
-    badge: {
-      title: '2',
-      translate: 'MENU.DASHBOARD.BADGE',
-      classes: 'badge-light-warning badge-pill'
-    },
-    children: [
-      {
-        id: 'analytics',
-        title: 'Analytics',
-        translate: 'MENU.DASHBOARD.ANALYTICS',
-        type: 'item',
-        role: ['Admin'], //? To set multiple role: ['Admin', 'Client']
-        icon: 'circle',
-        url: 'dashboard/analytics'
-      },
-      {
-        // If role is not assigned will be display to all
-        id: 'ecommerce',
-        title: 'eCommerce',
-        translate: 'MENU.DASHBOARD.ECOMMERCE',
-        type: 'item',
-        icon: 'circle',
-        url: 'dashboard/ecommerce'
-      }
-    ]
-  },
+  // {
+  //   id: 'dashboard',
+  //   title: 'Dashboard',
+  //   translate: 'MENU.DASHBOARD.COLLAPSIBLE',
+  //   type: 'collapsible',
+  //   // role: ['Admin'], //? To hide collapsible based on user role
+  //   icon: 'home',
+  //   badge: {
+  //     title: '2',
+  //     translate: 'MENU.DASHBOARD.BADGE',
+  //     classes: 'badge-light-warning badge-pill'
+  //   },
+  //   children: [
+  //     {
+  //       id: 'analytics',
+  //       title: 'Analytics',
+  //       translate: 'MENU.DASHBOARD.ANALYTICS',
+  //       type: 'item',
+  //       role: ['Admin'], //? To set multiple role: ['Admin', 'Client']
+  //       icon: 'circle',
+  //       url: 'dashboard/analytics'
+  //     },
+  //     {
+  //       // If role is not assigned will be display to all
+  //       id: 'ecommerce',
+  //       title: 'eCommerce',
+  //       translate: 'MENU.DASHBOARD.ECOMMERCE',
+  //       type: 'item',
+  //       icon: 'circle',
+  //       url: 'dashboard/ecommerce'
+  //     }
+  //   ]
+  // },
   // //Apps & Pages
   // {
   //   id: 'apps',
